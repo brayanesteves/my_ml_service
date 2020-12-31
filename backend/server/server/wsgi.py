a9c5bf20eefd852369f6a3740df03365b4dce662
Brayan Esteves <werneriasearch@gmail.com>
2020-12-31 13:29:01 -0400

    ab tests

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -23,5 +23,17 @@ try:
                             algorithm_description="Random Forest with simple pre- and post-processing",
                             algorithm_code=inspect.getsource(RandomForestClassifier))
 
+        # Extra Trees classifier
+    et = ExtraTreesClassifier()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="income_classifier",
+                            algorithm_object=et,
+                            algorithm_name="extra trees",
+                            algorithm_status="testing",
+                            algorithm_version="0.0.1",
+                            owner="Piotr",
+                            algorithm_description="Extra Trees with simple pre- and post-processing",
+                            algorithm_code=inspect.getsource(RandomForestClassifier))
+
 except Exception as e:
     print("Exception while loading the algorithms to the registry,", str(e))
